c4e100cb112b5774264b4fa749dea3d72fdf1224
Mathias Rieder <mathias.rieder@gmail.com>
2018-04-24 20:48:53 +0200

    implementation...

--- test/test_isogram.c
+++ test/test_isogram.c
@@ -2,18 +2,20 @@
 #include "../src/isogram.h"
 #include <stdlib.h>
 
-void test_empty_string(void)
+void test_single_characters(void)
 {
-   TEST_ASSERT_TRUE(is_isogram(""));
+   TEST_ASSERT_TRUE(is_isogram("a"));
+   TEST_ASSERT_TRUE(is_isogram("B"));
+   TEST_ASSERT_TRUE(is_isogram("z"));
 }
 
 int main(void)
 {
    UnityBegin("test/test_isogram.c");
 
-   RUN_TEST(test_empty_string);
-
+   RUN_TEST(test_single_characters);
 
    UnityEnd();
+
    return 0;
 }
