710141f99bbe62967f8a4f32965e9de3d54644d3
kepitto <hakan_kaya54@hotmail.com>
2020-05-06 09:10:35 +0000

    implemented

--- src/isogram.c
+++ src/isogram.c
@@ -1,7 +1,29 @@
 #include "isogram.h"
+#include "string.h"
 
 
 bool is_isogram(const char phrase[]){
+    // [b u t t e r]
+    
+    int len = strlen(phrase);
+    for(int i = 0; i < len; i++) {
+        char currentChar = phrase[i];
+        int hits = 0;
+        // check how many "currentChars" are there
+        for(int j=0; j < len; j++) {
+            if (currentChar == ' ') {
+                continue;
+            }
+            if ((currentChar == phrase[j]) || (currentChar == (phrase[j]+('a'-'A')))) {
+                hits++;
+            }
+        }
 
-    return false;
+        //if there's more than 1 currentChar, duplicate found
+        if (hits > 1) {
+            return false;
+        }
+    }
+
+    return true;
 }
--- test/test_isogram.c
+++ test/test_isogram.c
@@ -6,13 +6,44 @@ void test_empty_string(void)
 {
    TEST_ASSERT_TRUE(is_isogram(""));
 }
+void test_short_isogram_word(void)
+{
+   TEST_ASSERT_TRUE(is_isogram("Auto"));
+}
+void butter_is_no_isogram(void)
+{
+   TEST_ASSERT_FALSE(is_isogram("butter"));
+}
+void test_long_isogram(void)
+{
+   TEST_ASSERT_TRUE(is_isogram("Dialogschwerpunkt"));
+}
+void test_long_non_isogram(void)
+{
+   TEST_ASSERT_FALSE(is_isogram("abcdefghjiklmnopqrstuvwxyza"));
+}
+
+void upperCase_and_LowerCase_should_count_as_same_character(void) {
+    TEST_ASSERT_FALSE(is_isogram("Aal"));
+    TEST_ASSERT_FALSE(is_isogram("aAl"));
+}
+
+void spaces_should_not_count(void) {
+    TEST_ASSERT_TRUE(is_isogram("Dialog  schwerpunkt"));
+}
 
 int main(void)
 {
    UnityBegin("isIsogram");
 
    RUN_TEST(test_empty_string);
+   RUN_TEST(test_short_isogram_word);
+   RUN_TEST(butter_is_no_isogram);
+   RUN_TEST(test_long_isogram);
+   RUN_TEST(test_long_non_isogram);
 
+   RUN_TEST(upperCase_and_LowerCase_should_count_as_same_character);
+    RUN_TEST(spaces_should_not_count);
    UnityEnd();
    return 0;
 }
