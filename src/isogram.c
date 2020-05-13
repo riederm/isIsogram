4f600388ec33c7a3d5087193aec3fe8acd0b927e
mengxin19891107 <mengxin19891107@gmail.com>
2020-05-13 11:06:22 +0000

    extended implementation

--- src/isogram.c
+++ src/isogram.c
@@ -19,23 +19,56 @@
      ^ ^ ^ ^ // W?
        ^ ^ ^ // o?
 */
+
+// ignore everything but characters (a-z, A-Z) and digits (0-9)
+bool isIgnored(char lowerCaseC) {
+    bool isLowerChar = (lowerCaseC >= 'a' && lowerCaseC <= 'z');
+    bool isDigit = (lowerCaseC >= '0' && lowerCaseC <= '9');
+    return 
+        !(isLowerChar || isDigit);
+}
+
+char toLowerCase(char c){
+    if (c >= 'A' && c <= 'Z') {
+        return c - ('A' - 'a');
+    }
+    return c;
+}
+
 bool is_isogram(const char phrase[]){
+
+    int occurences[256] = {0};
+
     int len = strlen(phrase);
-    for(int i=0; i<len; i++){
-        char currentChar = phrase[i];
-        //check all other chars against currentChar
-        //find if there are more than 1 occurences
-        int occurences = 0;
-        for(int j=0; j<len; j++){
-            if(((phrase[j] == currentChar) && (currentChar !=' ')) || ((phrase[j]-currentChar) == ('a'-'A')) || ((phrase[j]-currentChar) == ('A'-'a'))){
-            //if(phrase[j] == currentChar){
-                occurences++;
-            }
-        }
-        if(occurences > 1){  //found duplicate characters --> no isogram
-            return false;
+    for (int i=0; i<len; i++) {
+        char currentChar = toLowerCase(phrase[i]);
+        if (!isIgnored(currentChar)){
+            occurences[currentChar]++;
         }
     }
-    //we didnt find duplicates, must be an isogram
+    //                          a b c
+    // occurences [ 0 0 0 ... 0 2 3 2 ... 0 0]
+    int oldNumber = -1;
+    for (int i=0; i<256; i++) {
+        int numberOfChar = occurences[i];
+        if (numberOfChar == 0){
+            //ignore
+        }else{
+            if (oldNumber == -1){
+                //case1 first number we see?
+                // --> remember as old number, so we can compare in the future
+                oldNumber = numberOfChar;    
+            }else {
+                //case2 we've already seen a number?
+                // --> compare to old number
+                if (numberOfChar != oldNumber) {
+                    //different occurences of characters
+                    return false; 
+                }
+            }
+        }
+    }
+
+    // we didnt find different occurences, must be an isogram
     return true;
 }
