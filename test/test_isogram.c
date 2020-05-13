6432f1a0f35000702b5873f661cb2a76a9ae9d25
mengxin19891107 <mengxin19891107@gmail.com>
2020-05-13 10:12:56 +0000

    second change

--- test/test_isogram.c
+++ test/test_isogram.c
@@ -2,42 +2,89 @@
 #include "../src/isogram.h"
 #include "vendor/unity_internals.h"
 #include <stdlib.h>
+
+//äquivalenzKlassen
+/*
+    - empty-string
+    - short strings
+    - long strings
+    - upper case / lower case
+    - strings with special characters (!?%)
+    - strings with numbers (digits 0-9)
+*/
+
+
 //this is a unit test
 void test_empty_string(void)
 {
-    //assert: erwartungshaltung
    TEST_ASSERT_TRUE(is_isogram(""));
 }
 
-void test_aa_is_no_isogram(){
-    bool result = is_isogram("aa");
-    TEST_ASSERT_FALSE(result);
+void test_short_strings() {
+    //positive tests
+    TEST_ASSERT_TRUE(is_isogram("a"));
+    TEST_ASSERT_TRUE(is_isogram("b"));
+    TEST_ASSERT_TRUE(is_isogram("c"));
+
+    TEST_ASSERT_TRUE(is_isogram("xyz"));
+    TEST_ASSERT_TRUE(is_isogram("abc"));
+    TEST_ASSERT_TRUE(is_isogram("def"));
+
+    //negative tests
+    TEST_ASSERT_FALSE(is_isogram("aa"));
+    TEST_ASSERT_FALSE(is_isogram("bb"));
+    TEST_ASSERT_FALSE(is_isogram("cc"));
 }
 
-void test_abc_is_an_isogram(){
-    bool result = is_isogram("abc");
-    TEST_ASSERT_TRUE(result);
-}
-
-void test_long_isograms(){
-    TEST_ASSERT_TRUE(is_isogram("abcdefghijklmnopqrstuvwxyz"));
+void test_long_strings() {
+    //positive tests
     TEST_ASSERT_TRUE(is_isogram("Dialogschwerpunkt"));
-    TEST_ASSERT_TRUE(is_isogram("Pokalbeschriftung"));
-   
+
+    //negative tests
+    TEST_ASSERT_FALSE(is_isogram("Dialogschwerpunkte"));
 }
 
-void test_long_non_isograms(){
-    TEST_ASSERT_FALSE(is_isogram("abcdefghijklmnopqrstuuvwxyz"));
-    TEST_ASSERT_FALSE(is_isogram("Pokalbescchriftung"));
+void test_upper_case_strings() {
+    //positive tests
+    TEST_ASSERT_TRUE(is_isogram("DIALOGSCHWERPUNKT"));
+    TEST_ASSERT_TRUE(is_isogram("ABC"));
+
+    //negative tests
+    TEST_ASSERT_FALSE(is_isogram("DIALOGSCHWERPUNKTE"));
+    TEST_ASSERT_FALSE(is_isogram("DEE"));
 }
 
-void test_upperLowerCase_should_not_matter(){
-    TEST_ASSERT_FALSE(is_isogram("abcdefghiIjklmnopqrstuvwxyz"));
-    TEST_ASSERT_FALSE(is_isogram("Dialogdschwerpunkt"));
+void test_lower_case_strings() {
+    //positive tests
+    TEST_ASSERT_TRUE(is_isogram("dialogschwerpunkt"));
+
+    //negative tests
+    TEST_ASSERT_FALSE(is_isogram("dialogschwerpunkte"));
 }
 
-void test_spaces_should_not_be_counted(){
-    TEST_ASSERT_TRUE(is_isogram("abc def ghi"));
+void test_mixed_case_strings() {
+     //positive tests
+    TEST_ASSERT_TRUE(is_isogram("DIaLOGScHWERpUNkT"));
+    TEST_ASSERT_TRUE(is_isogram("AbC"));
+
+    //negative tests
+    TEST_ASSERT_FALSE(is_isogram("DIALoGSCHWerPUNKTE"));
+    TEST_ASSERT_FALSE(is_isogram("DEe"));
+}
+
+void test_special_characters_strings(){
+    //positive tests
+    TEST_ASSERT_TRUE(is_isogram("!?%"));
+
+    //negative tests
+    TEST_ASSERT_FALSE(is_isogram("!??$"));
+}
+
+void test_digit_strings() {
+    //positive tests
+    TEST_ASSERT_TRUE(is_isogram("12345"));
+    //negative tests
+    TEST_ASSERT_FALSE(is_isogram("123451"));
 }
 
 int main(void)
@@ -45,12 +92,13 @@ int main(void)
    UnityBegin("isIsogram");
 
    RUN_TEST(test_empty_string);
-   RUN_TEST(test_aa_is_no_isogram);
-   RUN_TEST(test_abc_is_an_isogram);
-   RUN_TEST(test_long_isograms);
-   RUN_TEST(test_long_non_isograms);
-   RUN_TEST(test_upperLowerCase_should_not_matter);
-   RUN_TEST(test_spaces_should_not_be_counted);
+   RUN_TEST(test_short_strings);
+   RUN_TEST(test_long_strings);
+   RUN_TEST(test_upper_case_strings);
+   RUN_TEST(test_lower_case_strings);
+   RUN_TEST(test_mixed_case_strings);
+   RUN_TEST(test_special_characters_strings);
+   RUN_TEST(test_digit_strings);
 
    UnityEnd();
    return 0;
