1c76143fc10a235397bf71573a03f3cd25ab87a4
Patrick-Fiedler <privat@patrick-fiedler.de>
2020-05-13 11:04:25 +0000

    update from lecture

--- src/isogram.c
+++ src/isogram.c
@@ -18,20 +18,31 @@
         ^ ^ ^ // o?
 */
 
+
+bool isIgnored(char lowerCaseC) {
+    bool isLowerCHar = (lowerCaseC >= 'a' && lowerCaseC <= 'z');
+    bool isDigit = (lowerCaseC >= '0' && lowerCaseC <= '9');
+    return
+        !(isLowerCHar || isDigit);
+}
+
+char toLowerCase(char c){
+    if (c >= 'A' && c <= 'Z') {
+        return c - ('A' - 'a');
+    }
+    return c;
+}
+
 bool is_isogram(const char phrase[]){
     int len = strlen(phrase);
     for (int i=0; i<len; i++) {
-        
-        if(phrase[i] != 32){    //only check if not "space"
-            char currentChar = phrase[i];
+        char currentChar = toLowerCase(phrase[i]);
+        if (!isIgnored(currentChar)){
             //check all other chars against currentChar
             //find if there are more than 1 occurences
             int occurences = 0;
             for(int j=0; j<len; j++){
-                if ((currentChar >= 'A' ) && (currentChar <= 'Z' )){
-                    currentChar += 32;
-                }
-                if ((phrase[j] == currentChar) | (phrase[j] == currentChar - 32)) {
+                if (toLowerCase(phrase[j]) == currentChar) {
                     occurences++;
                 }
             }
@@ -43,3 +54,62 @@ bool is_isogram(const char phrase[]){
     // we didnt find duplicates, must be an isogram
     return true;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+MEINS
+bool is_isogram(const char phrase[]){
+    int len = strlen(phrase);
+    for (int i=0; i<len; i++) {
+        
+        if(phrase[i] != 32){    //only check if not "space"
+            char currentChar = phrase[i];
+            //check all other chars against currentChar
+            //find if there are more than 1 occurences
+            int occurences = 0;
+            for(int j=0; j<len; j++){
+                if(!(((currentChar >= 0) && (currentChar <= 47)) || ((currentChar >= 58) && (currentChar <= 64)) || ((currentChar >= 123) && (currentChar <= 127)))){
+                    if ((currentChar >= 'A' ) && (currentChar <= 'Z' )){
+                        currentChar += 32;
+                    }
+                    if ((phrase[j] == currentChar) | (phrase[j] == currentChar - 32)) {
+                        occurences++;
+                    }
+                }
+            }
+            if (occurences > 1) { //found duplicate characters --> no isogram
+                return false;
+            }
+        }
+    }
+    // we didnt find duplicates, must be an isogram
+    return true;
+}
+*/
--- test/test_isogram.c
+++ test/test_isogram.c
@@ -2,52 +2,123 @@
 #include "../src/isogram.h"
 #include <stdlib.h>
 
+//äquivalenzklassen
+/*
+    - empty string
+    - short strings
+    - long strings
+    - upper case / lower case
+    - strings with special characters (!?%)
+    - strings with numbers (digits 0-9) 
+
+
+
+*/
+
+
+
 //this is a unit test
-void test_empty_string(void)
-{
+void test_empty_string(void){
    TEST_ASSERT_TRUE(is_isogram(""));
 }
 
-void test_aa_is_no_isogram() {
-    bool result = is_isogram("aa");
-    TEST_ASSERT_FALSE(result);
+void test_short_strings(){
+    //positive tests
+    TEST_ASSERT_TRUE(is_isogram("a"));
+    TEST_ASSERT_TRUE(is_isogram("b"));
+    TEST_ASSERT_TRUE(is_isogram("c"));
+
+    TEST_ASSERT_TRUE(is_isogram("xyz"));
+    TEST_ASSERT_TRUE(is_isogram("abc"));
+    TEST_ASSERT_TRUE(is_isogram("def"));
+    
+    TEST_ASSERT_TRUE(is_isogram("aa"));
+    TEST_ASSERT_TRUE(is_isogram("aabb"));
+    TEST_ASSERT_TRUE(is_isogram("aabbcc"));
+
+    //negative tests
+    TEST_ASSERT_FALSE(is_isogram("aba"));
+    TEST_ASSERT_FALSE(is_isogram("aadbb"));
+    TEST_ASSERT_FALSE(is_isogram("aabbbcc"));
+
+
 }
 
-void test_abc_is_an_isogram() {
-    bool result = is_isogram("abc");
-    TEST_ASSERT_TRUE(result);
+void test_long_strings(){
+    //positive tests
+    TEST_ASSERT_TRUE(is_isogram("Dialogschwerpunkt"));
+    
+    //negative tests
+    TEST_ASSERT_FALSE(is_isogram("Heizölrückstossdämpfungsverwaltungsangestelltenbetreuer"));
 }
 
-void test_long_isograms() {
-   TEST_ASSERT_TRUE(is_isogram("abcdefghijklmnopqrstuvwxyz"));
-   TEST_ASSERT_TRUE(is_isogram("Dialogschwerpunkt"));
-   TEST_ASSERT_TRUE(is_isogram("Pokalbeschriftung"));
+void test_upper_Case_strings(){
+    //positive tests
+    TEST_ASSERT_TRUE(is_isogram("DIALOGSCHWERPUNKT"));
+    TEST_ASSERT_TRUE(is_isogram("ABC"));
+
+    //negative tests
+    TEST_ASSERT_FALSE(is_isogram("DIALOGSCHWERPUNKTE"));
+    TEST_ASSERT_FALSE(is_isogram("DEE"));
 }
 
-void test_long_non_isograms() {
-    TEST_ASSERT_FALSE(is_isogram("abcdezfghijklmnopqrstuvwxyz"));
-    TEST_ASSERT_FALSE(is_isogram("Pokalbescchriftung"));
+void test_lower_case_strings(){
+     //positive tests
+    TEST_ASSERT_TRUE(is_isogram("dialogschwerpunkt"));
+    
+    //negative tests
+    TEST_ASSERT_FALSE(is_isogram("heizölrückstossdämpfungsverwaltungsangestelltenbetreuer"));
 }
 
-void test_upperLowerCase_should_not_matter(){
-    TEST_ASSERT_FALSE(is_isogram("Dialogschdwerpunkt"));
+void test_mixed_strings(){
+    //positive tests
+    TEST_ASSERT_TRUE(is_isogram("DIaLOGSChWErPUnKT"));
+    TEST_ASSERT_TRUE(is_isogram("AbC"));
+
+    //negative tests
+    TEST_ASSERT_FALSE(is_isogram("DIaLoGSChWERPUnKTe"));
+    TEST_ASSERT_FALSE(is_isogram("DEe"));
 }
 
-void test_spaces_should_not_be_counted(){
-    TEST_ASSERT_TRUE(is_isogram("abc def ghi"));
+void test_special_character_strings(){
+    //positive tests
+    TEST_ASSERT_TRUE(is_isogram("!?%"));
+    TEST_ASSERT_TRUE(is_isogram("!??$"));
+    TEST_ASSERT_TRUE(is_isogram("abc??"));    
+    
+    //negativetests
+    TEST_ASSERT_FALSE(is_isogram("!a?&&&a?b$"));
+
 }
+
+void test_digit_strings(){
+    //positive tests
+    TEST_ASSERT_TRUE(is_isogram("12345"));
+
+    //negativetests
+    TEST_ASSERT_FALSE(is_isogram("123451"));
+}
+
+//TODO#1 specialcharacters are ignored is_isogram("abc!!") ==> TRUE 
+
+//TODO#2 extend definition of isisogram to extended definition 
+//          any number of occurences is fine, as long as its the same number
+//          e.g.: is_isogram("aaabbbccc") ==> TRUE (weil a=3, b=3, c=3)
+//                is_isogram("aaabbbcccc") ==> FALSE (weil a=3, b=3, c=4)
 
 int main(void)
 {
    UnityBegin("isIsogram");
 
-   RUN_TEST(test_empty_string);
-   RUN_TEST(test_aa_is_no_isogram);
-   RUN_TEST(test_abc_is_an_isogram);
-   RUN_TEST(test_long_isograms);
-   RUN_TEST(test_long_non_isograms);
-   RUN_TEST(test_upperLowerCase_should_not_matter);
-   RUN_TEST(test_spaces_should_not_be_counted);
+    RUN_TEST(test_empty_string);
+    RUN_TEST(test_short_strings);
+    RUN_TEST(test_long_strings);
+    RUN_TEST(test_upper_Case_strings);
+    RUN_TEST(test_lower_case_strings);
+    RUN_TEST(test_mixed_strings);
+    RUN_TEST(test_special_character_strings);
+    RUN_TEST(test_digit_strings);
+
 
 
    UnityEnd();
