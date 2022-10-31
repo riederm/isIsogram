aa648f95e58fd65050d2067049b1ab8abfc71f25
Lucas Huber <lucas.huber.1999@gmail.com>
2022-10-31 08:13:12 +0000

    Implement is_isogram
    add current charactor to array and check if character already in this array

--- src/isogram.c
+++ src/isogram.c
@@ -6,6 +6,14 @@
 
 
 bool is_isogram(const char phrase[]){
+    char char_check[10] = "";
+    for (int i = 0; phrase[i] != 0; i++) {
+        for(int j = 0; j < 10; i++) {
+            if(char_check[j] == 0) break;
+            if(char_check[j] == phrase[i]) return false;
+            char_check[i] = phrase[i];
+        }
+    }
 
     return true;
 }
