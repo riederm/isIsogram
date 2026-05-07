855c328db0117cd776a889bc38dfb3338a2eb79e
Anco21 <fa.coric@gmail.com>
2026-05-07 11:48:08 +0000

    Iogram geändert

--- src/isogram.c
+++ src/isogram.c
@@ -6,9 +6,26 @@
 
 
 bool is_isogram(const char phrase[]){
-    // this is the documentation
-    if (false) {
-        printf("hello world\n");
+
+    int i = 0;
+    int freq[256] = {0};
+
+    while(phrase[i] != '\0'){
+        char c = phrase[i];
+        freq[c]++;
+        i++;
     }
+    int max = 0;
+    for (int i = 0; i<256; i++) {
+	    if (freq[i] != 0) {
+		    //first time
+		    if(max == 0) {
+			    max = freq[i];
+		    } else if(freq[i] != max) {
+			    return false;
+		    }
+	}
+}
+
     return true;
 }
