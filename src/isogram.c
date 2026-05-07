8a2443b8752dba04e359385cbbfe09f2e998a6d3
simxn-u <simxn@gmx.at>
2026-05-07 13:31:42 +0200

    added implementation

--- src/isogram.c
+++ src/isogram.c
@@ -3,12 +3,40 @@
 #include "isogram.h"
 #include <string.h>
 #include <stdio.h>
+#include <ctype.h>
 
 
-bool is_isogram(const char phrase[]){
-    // this is the documentation
-    if (false) {
-        printf("hello world\n");
-    }
-    return true;
+bool is_isogram(const char phrase[])
+{
+  int counts[256] = {0};
+
+  for (size_t i = 0; phrase[i] != '\0'; i++) {
+      unsigned char c = (unsigned char)phrase[i];
+
+      if (!isalpha(c)) {
+          continue;
+      }
+
+      c = (unsigned char)tolower(c);
+      counts[c]++;
+  }
+
+  int expected = 0;
+
+  for (size_t i = 0; i < 256; i++) {
+      if (counts[i] == 0) {
+          continue;
+      }
+
+      if (expected == 0) {
+          expected = counts[i];
+          continue;
+      }
+
+      if (counts[i] != expected) {
+          return false;
+      }
+  }
+
+  return true;
 }
