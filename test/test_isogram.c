1b58619bfe96776d1d612052a3668027a46776f1
Mathias Rieder <mathias.rieder@gmail.com>
2026-05-21 09:10:26 +0000

    cleanup

--- test/test_isogram.c
+++ test/test_isogram.c
@@ -12,24 +12,10 @@ void test_empty_string(void) {
     TEST_ASSERT_TRUE(is_isogram("")); 
 }
 
-// this is a unit test
-void test_short_isograms(void) {
-    TEST_ASSERT_TRUE(is_isogram("abc"));
-    TEST_ASSERT_TRUE(is_isogram("opq"));
-}
-
-void test_short_non_isograms(void) {
-    TEST_ASSERT_FALSE(is_isogram("abca"));
-    TEST_ASSERT_FALSE(is_isogram("opqp"));
-}
-
-
 int main(void) {
     UnityBegin("isIsogram");
 
     RUN_TEST(test_empty_string);
-    RUN_TEST(test_short_isograms);
-    RUN_TEST(test_short_non_isograms);
 
     UnityEnd();
     return 0;
